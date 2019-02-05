cde280e8e59822a3112ec002c4b270446cf88bbd
Abiram López Hernández <abiiram10@gmail.com>
2019-02-05 14:26:51 -0600

    Actualiza

--- servicios/servicios.go
+++ servicios/servicios.go
@@ -1,10 +1,35 @@
 package servicios
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Inicia la ap
 func Inicia() {
-	fmt.Println("Hola")
+	r := mux.NewRouter()
+	r.HandleFunc("/", saluda).Methods("GET")
+	r.HandleFunc("/user/{id}", busca).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func saluda(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hola mundo!!.."))
+
+}
+func busca(w http.ResponseWriter, r *http.Request) {
+	Vars := mux.Vars(r)
+	userID := Vars["id"]
+	u1 := usuario.User{
+		ID:       0,
+		Edad:     30,
+		Nombre:   "Juan",
+		Apellido: "Perez",
+		Email:    "juan.Perez@gmail.com",
+	}
+	w.Write([]byte(userID))
+	json.NewEncoder(w).Encode(u1)
 }
