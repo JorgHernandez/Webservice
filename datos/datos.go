2584cf14fd41dec33c40f049d4a583e42139175c
Abiram López Hernández <abiiram10@gmail.com>
2019-02-05 16:59:31 -0600

    servidor y buscar

--- datos/datos.go
+++ datos/datos.go
@@ -17,8 +17,7 @@ var (
 func Inicia() (ErrDB error) {
 
 	var Error error
-	conexion, Error = obtenerConexion("192.168.0.1", 5432, "dev", "dev19", "alfa")
-
+	conexion, Error = obtenerConexion("192.168.13.15", 5432, "dev", "dev19", "alfa")
 	return Error
 }
 
@@ -37,4 +36,12 @@ func obtenerConexion(_DireccionIP string, _Puerto int, _User string, _Pass strin
 	}
 
 	return Conexion, ErrOpen
+}
+
+//Buscar Busca y muestra inforacion del usuario respecto al ID
+func Buscar(id string) *sql.Row {
+
+	sqlStatementSelect := `SELECT * FROM "Golang".users WHERE id=$1;`
+
+	return conexion.QueryRow(sqlStatementSelect, id)
 }
--- servicios/servicios.go
+++ servicios/servicios.go
@@ -1,7 +1,10 @@
 package servicios
 
 import (
+	"database/sql"
+	"elevenminds/web/webservice/datos"
 	usuario "elevenminds/web/webservice/entidades"
+	"elevenminds/web/webservice/utilidades"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -9,8 +12,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var tempemail, tempname, templastname string
+var tempid int
+
 // Inicia la ap
 func Inicia() {
+	utilidades.Inicia()
+
+	if Error := datos.Inicia(); Error != nil {
+		log.Fatalln(Error.Error())
+	}
 	r := mux.NewRouter()
 	r.HandleFunc("/", saluda).Methods("GET")
 	r.HandleFunc("/user/{id}", busca).Methods("GET")
@@ -24,13 +35,15 @@ func saluda(w http.ResponseWriter, r *http.Request) {
 func busca(w http.ResponseWriter, r *http.Request) {
 	Vars := mux.Vars(r)
 	userID := Vars["id"]
-	u1 := usuario.User{
-		ID:       0,
-		Edad:     30,
-		Nombre:   "Juan",
-		Apellido: "Perez",
-		Email:    "juan.Perez@gmail.com",
+	u1 := usuario.User{}
+	row := datos.Buscar(userID)
+
+	switch err := row.Scan(&u1.ID, &u1.Edad, &u1.Nombre, &u1.Apellido, &u1.Email); err {
+	case sql.ErrNoRows:
+		w.Write([]byte("Sin resultados"))
+	case nil:
+		json.NewEncoder(w).Encode(u1)
+	default:
+		panic(err)
 	}
-	w.Write([]byte(userID))
-	json.NewEncoder(w).Encode(u1)
 }
