22e13c1a88916bf62e3f4cdbe4a20b6cd7452e09
Jorge Hernandez <jorge_hdezdev@hotmail.com>
2019-02-07 11:52:20 -0600

    un conit

--- main.go
+++ main.go
@@ -8,11 +8,8 @@ import (
 //un comentario
 //ude.my/UC-42P4I2E7
 func main() {
-<<<<<<< HEAD
 	servicios.Inicia()
 	fmt.Println("Jorge")
-=======
 
 	fmt.Println("Hola")
->>>>>>> da519d1c8bffd90a21953b59d09934b282f660d9
 }
